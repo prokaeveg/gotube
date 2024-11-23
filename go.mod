d4cf17e2c39422a711fba398b06db96a35d96d3f
eprokaev <eprokaev@sigma-it.ru>
2024-11-23 15:56:56 +0300

    add Makefile and simple go-chi route for greet

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module gotube
 
 go 1.23.2
+
+require (
+	github.com/go-chi/chi/v5 v5.1.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/go-chi/chi/v5"
+	"net/http"
+)
+
+type Server struct {
+	Router *chi.Mux
+}
+
+func CreateServer() *Server {
+	server := &Server{
+		Router: chi.NewRouter(),
+	}
+
+	return server
+}
+
+func Greet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello World\n"))
+}
+
+func (server *Server) MountHandlers() {
+	server.Router.Get("/greet", Greet)
+}
 
 func main() {
-	fmt.Println("GoTube is running")
+	server := CreateServer()
+	server.MountHandlers()
+
+	fmt.Println("Server running on port 8080")
+
+	err := http.ListenAndServe(":8080", server.Router)
+
+	if err != nil {
+		panic(err)
+	}
 }
