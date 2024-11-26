7457ac92ecd400b17ebf1606efb738457b394bc5
eprokaev <prokaev96186903@gmail.com>
2024-11-26 19:32:47 +0300

    testing author

--- server/server.go
+++ server/server.go
@@ -17,9 +17,9 @@ type DBRepository struct {
 	DB *pgxpool.Pool
 }
 
+// @todo сделать хэширование пароля
 func (r *DBRepository) FindUserIdByCredentials(ctx context.Context, username string, password string) (int, error) {
 	var userID int
-	//@todo сделать хэширование пароля
 	err := r.DB.QueryRow(ctx, "SELECT id FROM users WHERE username = $1 AND password_hash = $2", username, password).Scan(&userID)
 	if err != nil {
 		return 0, err
